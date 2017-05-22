7665dcba255bd38546e16dc9d4f1a0f95499335f
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-21 23:00:03 -0400

    doing models

--- models/lang-model.js
+++ models/lang-model.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const QuA = require('./QuA-model.js');
 const langSchema = new Schema({
 tools:[{
   name:{type:String},
   Docs:{type:String},
   tutorials:[{
     whoPuttutorial:{type:String}}],
-  QuA:[{
-    authorOfQuestion:{type:String},
-    question:{type:String},
-    answer:{type:String},
-    upvotes:{type:Number},
-    problemPic:{type:String}
-  }],
-  author:
+  QuA:[QuA.schema],
+  author:{type:String},
+
 }],
 
 });
