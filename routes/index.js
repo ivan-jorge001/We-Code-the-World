12f8e2556ac6addedaa9b9adc5e8534f5042278f
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-06-02 10:28:40 -0400

    wokring on delete

--- routes/index.js
+++ routes/index.js
@@ -46,7 +46,7 @@ router.get('/', (req, res, next) => {
                     });
                 });
             });
-            
+
 
             setTimeout(function(d) {
               function rend(d) {
